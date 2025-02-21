e6037e2991c45454081050228d0d7d302ded7785
BossyRomain <romain.bossy@cea.fr>
2025-02-21 10:04:28 +0100

    (doc) add add's function documentation

--- app/mathematics.py
+++ app/mathematics.py
@@ -1,2 +1,11 @@
 def add(x: int, y: int) -> int:
+    """Addition of two integers
+
+    :param x: first operand
+    :type x: int
+    :param y: second operand
+    :type y: int
+    :return: the sum of x and y
+    :rtype: int
+    """
     return x + y
